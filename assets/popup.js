74a2612b2855d7d18b99b729ebd861b9bee478a9
Farkas János <farkasjanos.james@gmail.com>
2017-11-28 10:40:35 +0100

    Initial commit

---
+++ assets/popup.js
@@ -0,0 +1,239 @@
+// TODO: add confirm functionality
+// TODO: add reachedview, leftview functionality
+(function ($) {
+    $.JPopup = function (options) {
+        var defaults = {
+            verbose: false,
+            hidden: false,
+            content: '',
+            closeButton: '<i class="module-popup__close__button"></i>',
+            closeButtonPosition: 'right top',
+            textCenter: false,
+            maxWidth: false,
+            delay: null,
+            noiseTime: 5000
+        };
+
+        var plugin = this;
+        var opts = {},
+            noiseInterval = null;
+
+        plugin.settings = {};
+        plugin.dom = {};
+
+        plugin.init = function () {
+            getOptions();
+            if (!plugin.settings.content || plugin.settings.content === '') {
+                debug('No content specified. Exiting...');
+                return null;
+            }
+            createPopup();
+            setupPopup();
+        };
+
+        var debug = function (info, data) {
+                if (!info) {
+                    return;
+                }
+                if (plugin.settings.verbose === true) {
+                    if (data) {
+                        console.log(info, data);
+                    } else {
+                        console.log(info);
+                    }
+                }
+            },
+            isDomElement = function () {
+                return (options.length === 1 && options[0].dataset);
+            },
+            parseTime =function (time) {
+                if ($.isNumeric(time)) {
+                    return parseInt(time, 10);
+                } else if (time.match(/^\d+(ms|MS|Ms)$/g)) {
+                    return parseInt(time.match(/\d+/g), 10);
+                } else if (time.match(/^\d+(s|S)$/g)) {
+                    return parseInt(time.match(/\d+/g), 10) * 1000;
+                } else {
+                    return null;
+                }
+            },
+            getOptions = function () {
+                if (isDomElement()) {
+                    opts = options.html5data('jpopup');
+                } else {
+                    opts = options;
+                }
+                plugin.settings = $.extend({}, defaults, opts);
+
+                debug('\r\nCreated a new instance of Popup with:\r\n', options);
+                debug('Given options:\r\n', opts);
+
+                if (plugin.settings.delay !== null) {
+                    var delay = parseTime(plugin.settings.delay);
+
+                    if (delay) {
+                        plugin.settings.hidden = true;
+                        plugin.settings.delay = delay;
+                        debug('Got \'delay\' so \'hidden\' set to true.');
+                    }
+                }
+
+                if (plugin.settings.noiseTime !== null) {
+                    var noiseTime = parseTime(plugin.settings.noiseTime);
+
+                    if (noiseTime) {
+                        plugin.settings.noiseTime = noiseTime;
+                        debug('Got \'noiseTime\'.');
+                    }
+                }
+
+                debug('Merged configuration:\r\n', plugin.settings);
+                debug('Verbose enabled!');
+                debug('Options stored...');
+            },
+            displayPopup = function (delay) {
+                var $activePopups = $('.module-popup').not($('.module--hidden'));
+                if (noiseInterval) {
+                    clearInterval(noiseInterval);
+                }
+                if ($activePopups.length) {
+                    noiseInterval = setInterval(displayPopup, plugin.settings.noiseTime);
+                } else {
+                    if (delay) {
+                        setTimeout(function () {
+                            plugin.dom.popup.removeClass('module--hidden');
+                        }, delay);
+
+                        debug('Displaying popup in: ' + delay + 'ms');
+                    } else {
+                        plugin.dom.popup.removeClass('module--hidden');
+                        debug('Displaying popup...');
+                    }
+                }
+            },
+            initEventListeners = function () {
+                debug('Adding event listeners...');
+
+                $(document)
+                    .off('click.Popup', '#' + plugin.dom.popup.attr('id'))
+                    .on('click.Popup', '#' + plugin.dom.popup.attr('id'), function () {
+                        $(this).addClass('module--hidden');
+                    })
+                    .find(plugin.dom.popup_content_wrapper)
+                    .off('click.Popup')
+                    .on('click.Popup', function () {
+                        return false;
+                    });
+                debug('Close listener added');
+            },
+            makeID = function () {
+                var id = '',
+                    availableChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                for (var i = 0; i < 10; i++) {
+                    id += availableChars.charAt(Math.floor(Math.random() * availableChars.length));
+                }
+                return id;
+            },
+            createPopup = function () {
+                plugin.dom.popup = $('<div>')
+                    .addClass('module-popup module--hidden')
+                    .attr('id', 'popup-' + makeID());
+
+                plugin.dom.popup_wrapper = $('<div>')
+                    .addClass('module-popup__wrapper')
+                    .appendTo(plugin.dom.popup);
+
+                plugin.dom.popup_container = $('<div>')
+                    .addClass('container')
+                    .appendTo(plugin.dom.popup_wrapper);
+
+                plugin.dom.popup_content_wrapper = $('<div>')
+                    .addClass('module-popup__content-wrapper')
+                    .appendTo(plugin.dom.popup_container);
+
+                plugin.dom.popup_content = $('<div>')
+                    .addClass('module-popup__content')
+                    .appendTo(plugin.dom.popup_content_wrapper);
+
+                plugin.dom.popup_close = $('<div>')
+                    .addClass('module-popup__close')
+                    .appendTo(plugin.dom.popup);
+
+                plugin.dom.popup.appendTo($('body'));
+
+                debug('Popup initialized!');
+
+                initEventListeners();
+            },
+            setupPopup = function () {
+                var config = plugin.settings;
+                plugin.dom.popup_content.html(config.content);
+
+                plugin.dom.popup_close.html(config.closeButton);
+
+                if (config.closeButtonPosition === 'right top' || config.closeButtonPosition === 'top right') {
+                    plugin.dom.popup_close.addClass('module-popup__close--right-top');
+                } else if (config.closeButtonPosition === 'right bottom' || config.closeButtonPosition === 'bottom right') {
+                    plugin.dom.popup_close.addClass('module-popup__close--right-bottom');
+                } else if (config.closeButtonPosition === 'left top' || config.closeButtonPosition === 'top left') {
+                    plugin.dom.popup_close.addClass('module-popup__close--left-top');
+                } else if (config.closeButtonPosition === 'left bottom' || config.closeButtonPosition === 'bottom left') {
+                    plugin.dom.popup_close.addClass('module-popup__close--left-bottom');
+                }
+
+                if (config.textCenter === true) {
+                    plugin.dom.popup_content.addClass('module-popup__content--text-center');
+                }
+
+                if (config.maxWidth === true) {
+                    plugin.dom.popup_content_wrapper.addClass('module-popup__content-wrapper--max-width');
+                }
+
+                if (config.delay) {
+                    displayPopup(config.delay);
+                }
+
+                if (config.hidden !== true) {
+                    displayPopup();
+                }
+            };
+
+        plugin.init();
+    };
+
+    $.fn.JPopup = function (options) {
+        return this.each(function () {
+            if (undefined == $(this).data('JPopup')) {
+                var plugin = new $.JPopup(this, options);
+                $(this).data('JPopup', plugin);
+            }
+        });
+    };
+
+    // Getting elements with data-jpopup
+    $(function () {
+        var $popups = $('[data-jpopup]');
+        $.each($popups, function () {
+            $.JPopup($(this));
+        });
+    });
+
+})(jQuery);
+
+(function (window, document, $) {
+    $(function () {
+        $.JPopup({
+            // verbose: true,
+            // hidden: false,
+            content: 'Popup triggered by JavaScript'
+            // closeButton: '<i class="sdfdf"></i>',
+            // closeButtonPosition: 'left bottom',
+            // textCenter: true,
+            // maxWidth: true,
+            // delay: '3s',
+            // inView: true,
+            // noiseTime: 3000,
+        });
+    });
+
+})(window, document, jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,242 @@
+var gulp = require('gulp'),
+    del = require('del'),
+    assetBuilder = require('asset-builder'),
+    minimist = require('minimist'),
+    gulpIf = require('gulp-if'),
+    scss = require('postcss-scss'),
+    sass= require('gulp-sass'),
+    cssnano= require('cssnano'),
+    plumber = require('gulp-plumber'),
+    postcss = require('gulp-postcss'),
+    styleLint= require('stylelint'),
+    postcssReporter = require('postcss-reporter'),
+    lazypipe = require('lazypipe'),
+    sourceMaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    mergeStream = require('merge-stream'),
+    jshint = require('gulp-jshint'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    runSequence = require('run-sequence'),
+    gutil = require('gulp-util');
+
+cli = minimist(process.argv.slice(2));
+
+manifest = assetBuilder('manifest.json');
+
+// Command line option:
+//  --fatal=[warning|error|off]
+fatalError = cli.production || cli.fatal;
+
+// Handle an error based on its severity level.
+// Log all levels, and exit the process for fatal levels.
+function handleError(error) {
+    gutil.log(error.message);
+    if (fatalError) {
+        console.log('KILLING PROCESS, SORRY');
+        process.exit(1);
+    }
+}
+
+/**
+ * CSS
+ */
+// Options
+const $reporter_options = {
+        clearReportedMessages: true
+    },
+    $postcss_options = {
+        syntax: scss
+    };
+
+// ### Stylelint
+// `gulp stylelint` - Run stylelint on scss
+gulp.task('stylelint', function () {
+    return gulp.src([manifest.paths.source + '*.scss'])
+        .pipe(gulpIf(!cli.production,
+            plumber()
+        ))
+        .pipe(postcss(
+            [
+                styleLint,
+                postcssReporter($reporter_options)
+            ],
+            $postcss_options
+        ));
+});
+
+// Options and build function
+const $css_options = {
+        cssnano: {
+            safe: true
+        },
+        sass: {
+            outputStyle: 'nested',
+            precision: 5,
+            includePaths: ['.'],
+            errLogToConsole: !cli.production
+        },
+        autoPrefixer: {
+            browsers: [
+                'last 2 versions',
+                'android 4'
+            ]
+        },
+        reporter: {
+            clearMessages: true
+        }
+    },
+    cssTasks = function (filename) {
+        return lazypipe()
+            .pipe(function () {
+                return gulpIf(!cli.production,
+                    plumber()
+                );
+            })
+            .pipe(function () {
+                return gulpIf(!cli.production,
+                    sourceMaps.init()
+                );
+            })
+            .pipe(function () {
+                return gulpIf('*.scss',
+                    sass($css_options.sass)
+                );
+            })
+            .pipe(concat, filename)
+            .pipe(
+                autoprefixer, $css_options.autoPrefixer
+            )
+            .pipe(function () {
+                return gulpIf(cli.production,
+                    postcss([
+                            cssnano($css_options.cssnano)
+                        ]
+                    )
+                );
+            })
+            .pipe(postcss, [
+                postcssReporter($css_options.reporter)
+            ])
+            .pipe(function () {
+                return gulpIf(!cli.production,
+                    sourceMaps.write('.')
+                );
+            })();
+    };
+
+// ### Styles
+// `gulp styles` - Compiles, combines, and optimizes Bower CSS and project CSS.
+// By default this task will only log a warning if a precompiler error is
+// raised. If the `--production` flag is set: this task will fail outright.
+gulp.task('styles', ['stylelint'], function () {
+    const $merged = mergeStream();
+
+    $merged.on('error', function () {
+        this.emit('end');
+    });
+
+    manifest.forEachDependency('css', function (dep) {
+        const cssTasksInstance = cssTasks(dep.name);
+
+        if (!cli.production) {
+            cssTasksInstance.on('error', handleError);
+        }
+
+        $merged.add(
+            gulp.src(dep.globs)
+                .pipe(cssTasksInstance)
+        );
+    });
+
+    return $merged.pipe(writeToManifest());
+});
+
+// Write to rev manifest
+// If there are any revisioned files then write them to the rev manifest.
+// See https://github.com/sindresorhus/gulp-rev
+function writeToManifest () {
+    return lazypipe()
+        .pipe(gulp.dest, manifest.paths.dist)();
+}
+
+/**
+ * JS
+ */
+// Options
+const sourceFiles = [
+        'bower.json',
+        'gulpfile.js'
+    ],
+    $jsHint_options = {
+        esversion: 6
+    };
+
+// ### JSHint
+// `gulp jshint` - Lints configuration JSON and project JS.
+// Error: Cannot find module 'jshint/src/cli'
+gulp.task('jshint', function () {
+    return gulp.src(sourceFiles.concat(manifest.getProjectGlobs().js))
+        .pipe(jshint($jsHint_options))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(gulpIf(cli.production,
+            jshint.reporter('fail')
+            )
+        );
+});
+
+// Build function
+const jsTasks = function (filename) {
+    return lazypipe()
+        .pipe(function () {
+            return gulpIf(!cli.production,
+                sourceMaps.init()
+            );
+        })
+        .pipe(babel)
+        .pipe(concat, filename)
+        .pipe(function () {
+            return gulpIf(cli.production,
+                uglify()
+            );
+        })
+        .pipe(function () {
+            return gulpIf(!cli.production,
+                sourceMaps.write('.')
+            );
+        })();
+};
+
+// ### Scripts
+// `gulp scripts` - Runs JSHint and then compiles, combines,
+// and optimizes Bower JS and project JS.
+gulp.task('scripts', ['jshint'], function () {
+    const $merged = mergeStream();
+
+    $merged.on('error', function () {
+        this.emit('end');
+    });
+
+    manifest.forEachDependency('js', function (dep) {
+        $merged.add(
+            gulp.src(dep.globs)
+                .pipe(jsTasks(dep.name))
+                .on('error', handleError)
+        );
+    });
+    return $merged.pipe(writeToManifest());
+});
+
+// ### Clean
+// `gulp clean` - Deletes the dist folder entirely.
+gulp.task('clean', function (callback) {
+    return del(manifest.paths.dist, callback);
+});
+
+gulp.task('build', ['clean'], function (callback) {
+    runSequence('styles', 'scripts', callback);
+});
+
+gulp.task('default', ['build'], function () {
+});
